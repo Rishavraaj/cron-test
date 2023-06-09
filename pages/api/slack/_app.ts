3aee73df0ca84759d21fae4e2e27d6b3bc98065c
Rishavraaj <rishavrajpatna7@gmail.com>
2023-06-09 14:08:01 +0530

    cron

--- pages/api/slack/_app.ts
+++ pages/api/slack/_app.ts
@@ -1,42 +1,42 @@
-import { App, LogLevel } from "@slack/bolt";
-import { WebClient } from "@slack/web-api";
-import { AppRunner } from "@seratch_/bolt-http-runner";
-import sendToSlack from "@/utils/sendToslack";
-import { formatData } from "@/utils/TaskNotificationFormatter";
+// import { App, LogLevel } from "@slack/bolt";
+// import { WebClient } from "@slack/web-api";
+// import { AppRunner } from "@seratch_/bolt-http-runner";
+// import sendToSlack from "@/utils/sendToslack";
+// import { formatData } from "@/utils/TaskNotificationFormatter";
 import cron from "node-cron";
 
-export const appRunner = new AppRunner({
-  logLevel: LogLevel.DEBUG,
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET as string,
-});
+// export const appRunner = new AppRunner({
+//   logLevel: LogLevel.DEBUG,
+//   token: process.env.SLACK_BOT_TOKEN,
+//   signingSecret: process.env.SLACK_SIGNING_SECRET as string,
+// });
 
-const app = new App(appRunner.appOptions());
+// const app = new App(appRunner.appOptions());
 
-app.event("app_mention", async ({ say }) => {
-  console.log("app_mention event received");
-  // debugger; // Pause execution here
-  await say("yes");
-  console.log("Message sent");
-});
+// app.event("app_mention", async ({ say }) => {
+//   console.log("app_mention event received");
+//   // debugger; // Pause execution here
+//   await say("yes");
+//   console.log("Message sent");
+// });
 
-const sendSlackMessage = async () => {
-  console.log("running");
-  try {
-    const data = await sendToSlack();
-    const web = new WebClient(process.env.SLACK_BOT_TOKEN);
-    const formattedMessage = formatData(data);
-    console.log(formattedMessage);
-    await web.chat.postMessage({
-      channel: "C05BC103SM60",
-      text: formattedMessage,
-    });
+// const sendSlackMessage = async () => {
+//   console.log("running");
+//   try {
+//     const data = await sendToSlack();
+//     const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+//     const formattedMessage = formatData(data);
+//     console.log(formattedMessage);
+//     await web.chat.postMessage({
+//       channel: "C05BC103SM60",
+//       text: formattedMessage,
+//     });
 
-    console.log("Data sent to Slack");
-  } catch (error) {
-    console.error("Error sending data to Slack:", error);
-  }
-};
+//     console.log("Data sent to Slack");
+//   } catch (error) {
+//     console.error("Error sending data to Slack:", error);
+//   }
+// };
 
 const test = () => {
   console.log("cron is running");
@@ -44,6 +44,6 @@ const test = () => {
 
 cron.schedule("* * * * * *", test);
 
-cron.schedule("* * * * *", sendSlackMessage);
+// cron.schedule("* * * * *", sendSlackMessage);
 
-appRunner.setup(app);
+// appRunner.setup(app);
--- pages/api/slack/events.ts
+++ pages/api/slack/events.ts
@@ -1,21 +1,21 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { appRunner } from "./_app";
+// import { NextApiRequest, NextApiResponse } from "next";
+// // import { appRunner } from "./_app";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res
-      .status(405)
-      .json({ error: "Sorry! This endpoint does not accept your requests." });
-    return;
-  }
-  await appRunner.handleEvents(req, res);
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     res
+//       .status(405)
+//       .json({ error: "Sorry! This endpoint does not accept your requests." });
+//     return;
+//   }
+//   await appRunner.handleEvents(req, res);
+// }
