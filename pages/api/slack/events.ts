b54927a09ec5ddcfd673159f0be4a7e8f039df6f
Rishavraaj <rishavrajpatna7@gmail.com>
2023-06-12 13:13:44 +0530

    change

--- pages/api/slack/events.ts
+++ pages/api/slack/events.ts
@@ -11,8 +11,6 @@ export const config = {
   },
 };
 
-let isDataSentToSlack = false;
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -23,39 +21,6 @@ export default async function handler(
       .json({ error: "Sorry! This endpoint does not accept your requests." });
     return;
   }
-  // try {
-  //   console.log("running");
-
-  //   // Check if data has already been sent
-  //   if (isDataSentToSlack) {
-  //     console.log("Data already sent to Slack");
-  //     res.status(200).json({ message: "Data already sent to Slack" });
-  //     return;
-  //   }
-
-  //   const data = await sendToSlack(); // Fetch the data
-  //   console.log("fetching data");
-
-  //   const web = new WebClient(process.env.SLACK_BOT_TOKEN);
-  //   const formattedMessage = formatData(data);
-  //   console.log("data fetched");
-  //   console.log(formattedMessage);
-  //   await web.chat.postMessage({
-  //     channel: "C05BC103SM6l",
-  //     text: formattedMessage,
-  //   });
-
-  //   console.log("Data sent to Slack");
-  //   res.status(200).json({ message: "Data sent to Slack" });
-
-  //   isDataSentToSlack = true; // Set the flag to indicate data has been sent
-  // } catch (error) {
-  //   console.error("Error sending data to Slack:", error);
-  //   res.status(500).json({ error: "Error sending data to Slack" });
-  // }
-
-  console.log("Qstash is running");
-  console.log(req.headers);
 
   await appRunner.handleEvents(req, res);
 }
