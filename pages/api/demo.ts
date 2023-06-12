c5ef492f6ce9e92b682151265ac302b86bf9676b
Rishavraaj <rishavrajpatna7@gmail.com>
2023-06-12 14:24:40 +0530

    change

--- pages/api/demo.ts
+++ pages/api/demo.ts
@@ -1,17 +1,23 @@
 import { consoleSchedule } from "@/utils/Tasks";
 import type { NextApiRequest, NextApiResponse } from "next";
 import cron from "node-cron";
-import { sendSlackMessage } from "./slack/_app";
+import { WebClient } from "@slack/web-api";
+// import { sendSlackMessage } from "./slack/_app";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    await sendSlackMessage();
-    res.status(200);
+    const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+
+    await web.chat.postMessage({
+      channel: "C05BC103SM6",
+      text: "test msg",
+    });
+    res.status(200).send("data sent to slack");
+    console.log("Data sent to Slack");
   } catch (error) {
-    console.log(error);
+    console.error("Error sending data to Slack:", error);
   }
-  // res.status(200).json({ message: "Server is healthy" });
 }
